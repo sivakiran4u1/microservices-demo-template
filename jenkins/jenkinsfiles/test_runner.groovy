17a28f317974309d049ec8013f8c0b7fee3feebc
AhmadSealights <ahmad.wahbi@sealights.io>
2024-03-10 12:05:36 +0200

    change test runner for robot

--- jenkins/jenkinsfiles/test_runner.groovy
+++ jenkins/jenkinsfiles/test_runner.groovy
@@ -151,6 +151,26 @@ pipeline {
                       export machine_dns="${env.MACHINE_DNS}"
                       echo 'robot framework starting ..... '
                       cd ./integration-tests/robot-tests
+                      
+                      apt-get update && apt-get install -y \
+                        ca-certificates \
+                        fonts-liberation \
+                        libappindicator3-1 \
+                        libasound2 \
+                        libatk-bridge2.0-0 \
+                        libatk1.0-0 \
+                        libcups2 \
+                        libdbus-1-3 \
+                        libgdk-pixbuf2.0-0 \
+                        libnspr4 \
+                        libnss3 \
+                        libxcomposite1 \
+                        libxdamage1 \
+                        libxrandr2 \
+                        xdg-utils \
+                        libgbm1
+                        wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+                      apt install -y ./google-chrome-stable_current_amd64.deb
                       pip install -r requirements.txt
                       robot --listener "SLListener.py:${env.SL_TOKEN}::Robot Tests:${params.SL_LABID}" ./
                       cd ../..
