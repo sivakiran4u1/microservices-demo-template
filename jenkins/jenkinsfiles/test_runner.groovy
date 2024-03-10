cd1c7b75c8bd7661e7545a43e34afaa35ece9207
AhmadSealights <ahmad.wahbi@sealights.io>
2024-03-10 10:44:21 +0200

    changed robot name

--- jenkins/jenkinsfiles/test_runner.groovy
+++ jenkins/jenkinsfiles/test_runner.groovy
@@ -151,7 +151,7 @@ pipeline {
                       export machine_dns="${env.MACHINE_DNS}"
                       echo 'robot framework starting ..... '
                       cd ./integration-tests/robot-tests
-                      robot --listener "SLListener.py:${SL_TOKEN}::Robot Tests selenium:${SL_LAB_ID}" ./
+                      robot --listener "SLListener.py:${SL_TOKEN}::Robot Tests:${SL_LAB_ID}" ./
                       cd ../..
                       sleep ${env.wait_time}
                       """
