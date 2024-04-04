5173c45e1d6684e6753ebee583f44c3294f07486
AhmadSealights <ahmad.wahbi@sealights.io>
2024-04-04 12:35:49 +0300

    .

--- integration-tests/python-tests/python-tests.py
+++ integration-tests/python-tests/python-tests.py
@@ -112,5 +112,35 @@ def test_checkout(r=requests):
         response = r.post(BASE_URL + "/cart/checkout", data=data)
         assert response.status_code == 200
 
+numbers=["1","4","5"]
+def test_added():
+    response = requests.get(BASE_URL + "/added")
+    print(response.text)
+    assert response.status_code == 200
+
+def test_added_num():
+    for num in numbers:
+        response = requests.get(BASE_URL + "/added/number/"+num)
+        print(response.text)
+        assert response.status_code == 200
+
+def test_added_content_get():
+    response = requests.get(BASE_URL + "/added/content")
+    assert response.status_code == 200
+
+def test_added_content_delete():
+    response = requests.delete(BASE_URL + "/added/content")
+    assert response.status_code == 200
+
+def test_added_content_post():
+    for num in numbers:
+        data = {
+            'num': num,
+        }
+        response = requests.get(BASE_URL + "/added/content",data=data)
+        print(response.text)
+        assert response.status_code == 200
+
+
 if __name__ == "__main__":
     pytest.main()
